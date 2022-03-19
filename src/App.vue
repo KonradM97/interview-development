7e7b5a2bfc0706a57d131ab0a2cc62c8ea235a8d
KonradM97 <konrad.fazi230@gmail.com>
2022-03-19 19:43:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="app">
+        <div class="header">
+            <button @click="switchVisibility">{{ buttonName }}</button>
+        </div>
+        <workers-list v-if="isListVisible" :workers-list="workers"/>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import WorkersList from './components/WorkersList.vue';
+import WorkerListFactory from './models/WorkerListFactory';
+
+@Component({
+    name: 'App',
+    components: {WorkersList},
+})
+export default class App extends Vue {
+
+    data(){
+        return {
+            workers: [],
+            buttonName: 'Pokaż listę pracowników',
+            isListVisible: false,
+        };
+    }
+
+    mounted(){
+        this.createWorkers();
+    }
+    switchVisibility(){
+        this.isListVisible = !this.isListVisible;
+        this.isListVisible? this.buttonName =  'Schowaj listę pracowników' : this.buttonName =  'Pokaż listę pracowników';
+    }
+    async createWorkers(){
+        let factory = new WorkerListFactory();
+        this.workers = await factory.getWorkers();
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./assets/global.css";
+
+#app {
+    .header {
+        padding: 0.5rem;
+    }
+}
+</style>
---
+++ src/components/WorkersList.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="workers-list">
+    Docelowa lista pracowników
+    <table>
+      <tbody>
+        <tr v-for="worker in workersList" :key="worker.id" v-bind:style="[worker._age == minAge ? {'background': '#0F0'}: worker._age == maxAge ? {'background': '#F00'}:{'background': '#FFF'}]">
+          <td>{{ worker._firstName }} {{ worker._lastName }}</td>
+          <td>{{ worker._age }}</td>
+          <td>{{ worker._salary }}</td>
+        </tr>
+      </tbody>
+    </table>
+    Średnia pensja: {{ averageSalary }} <br />
+    Maksymalny wiek: {{ maxAge }}<br />
+    Minimalny wiek: {{ minAge }}
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+@Component({ name: "WorkersList" })
+export default class WorkersList extends Vue {
+  @Prop({ type: Array, required: false, default: () => [] }) workersList;
+  data() {
+    return {
+      averageSalary: 0,
+      minAge: 0,
+      maxAge: 0
+    };
+  }
+  mounted() {
+    this.averageSalary = this.getAverageSalary();
+    this.minAge = this.getMinAge();
+    this.maxAge = this.getMaxAge();
+  }
+  getAverageSalary() {
+    let lenght = Object.keys(this.workersList).length;
+    let sum = 0;
+    for (var i = 0; i < lenght; i++) {
+      sum += this.workersList[i]._salary;
+    }
+    if (lenght != 0) {
+      return sum / lenght;
+    } else {
+      return 0;
+    }
+  }
+  getMinAge() {
+    const ages = this.workersList.map((a) => a._age);
+    return Math.min.apply(null, ages);
+  }
+  getMaxAge() {
+    const ages = this.workersList.map((a) => a._age);
+    return Math.max.apply(null, ages);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.workers-list {
+  padding: 0.5rem;
+}
+td {
+  padding-right: 0.5rem;
+}
+</style>
